b92ebcee37acc50e72877042689eea8c5d98b71e
Aaditya Trivedi <adityatrivedi972@gmail.com>
2026-01-16 10:53:44 +0530

    Fix shop migrations by registering dynamic databases for Django commands

--- cloth_rental_saas/master/utils.py
+++ cloth_rental_saas/master/utils.py
@@ -1,23 +1,31 @@
 import os
+import copy
 from django.conf import settings
 from django.core.management import call_command
-from cloth_rental_saas.db_config import load_shop_databases
 
 def create_shop_database(db_name):
     """
-    Create SQLite DB file and apply migrations
+    Create SQLite DB file, register it in settings, and apply migrations
     """
-    db_path = settings.BASE_DIR / 'shop_dbs' / f"{db_name}.sqlite3"
 
-    if not os.path.exists(db_path):
-        open(db_path, 'a').close()
+    shop_db_path = settings.BASE_DIR / 'shop_dbs'
+    os.makedirs(shop_db_path, exist_ok=True)
 
-    # Reload DB configs before migrate
-    load_shop_databases()
+    db_file = shop_db_path / f"{db_name}.sqlite3"
 
+    if not os.path.exists(db_file):
+        open(db_file, 'a').close()
+
+    # 🔑 REGISTER DB TEMPORARILY FOR MANAGEMENT COMMANDS
+    default_db = settings.DATABASES['default']
+    settings.DATABASES[db_name] = copy.deepcopy(default_db)
+    settings.DATABASES[db_name]['NAME'] = db_file
+
+    # APPLY MIGRATIONS
     call_command(
         'migrate',
+        'shop',
         database=db_name,
         interactive=False,
-        verbosity=0
+        verbosity=1
     )
